8663654bc4a5d10fd4d0ac4e62d5aa107491529b
PATELCHIRAG4597 <124140359+PATELCHIRAG4597@users.noreply.github.com>
2023-02-23 15:43:55 +0530

    UITableView-Grouped demo udemi application

--- uiTableviewgrouped/ViewController.swift
+++ uiTableviewgrouped/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    @IBOutlet weak var Name: UITextField!
+    
+    @IBOutlet weak var Password: UITextField!
+    @IBOutlet weak var Username: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func Button(_ sender: UISwitch) {
+    if sender.isOn{
+        print("button is ON ")
+    }else{
+        print("button is OFF")
+        }
+    }
+    
+    @IBAction func Stepper(_ sender: UIStepper) {
+        print(sender.value.description)
+        
+    }
+    //    @IBAction func Stepper(_ sender: UIStepper) {
+//
+//    }
+    
+    @IBAction func Segment(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex{
+        case 0:
+            print("segmnet equals 0")
+            break
+        case 1:
+            print("segmnet equals 1")
+            break
+        default:
+            break
+        }
+        
+    }
+    
+    
 }
 
